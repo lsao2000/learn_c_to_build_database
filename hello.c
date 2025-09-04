420259990802b07578909670490482104834af15
lsao2000 <lahcenenligne@gmail.com>
2025-09-04 12:39:47 +0100

    ...

--- hello.c
+++ hello.c
@@ -1,7 +1,18 @@
-// #include <stdio.h>
-int main(void) {
-    int lstNumber[5];
-    // int  listSize = si;
-  return 0;
-}
+// // #include <stdio.h>
+// int main(void) {
+//
+//   return 0;
+// }
+#include <stdio.h>
 
+int main() {
+    int x = 10;        // Normal integer variable
+    int *ptr = &x;     // Pointer variable storing address of x
+
+    printf("Value of x: %d\n", x);
+    printf("Address of x: %p\n", &x);
+    printf("Value of ptr: %p\n", ptr);
+    printf("Value at address stored in ptr: %d\n", *ptr);
+
+    return 0;
+}
