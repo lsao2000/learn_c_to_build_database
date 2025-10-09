1899ac2a56c1d6ddfad972119167a7ae04df41f1
lsao2000 <lahcenenligne@gmail.com>
2025-10-09 10:45:37 +0100

    ...

--- arrays_pointers.c
+++ arrays_pointers.c
@@ -1,4 +1,31 @@
-int main(){
+#include <stdio.h>
+int addition(int a, int b);
+int substraction(int a, int b);
 
-    return 0;
+int main() {
+  int numbers[] = {3, 39, 393, 93, 1, 0, 14};
+  for (int i = 0; i < (sizeof(numbers) / sizeof(numbers[0])); i++) {
+    printf("Number %d: %d\n", i, numbers[i]);
+  }
+  int *numP = &numbers[0];
+  numP[0] = 5;
+  numP[0 + 1] = 5;
+  numP[0 + 2] = 5;
+  numP[0 + 3] = 5;
+  numP[0 + 4] = 5;
+
+  for (int i = 0; i < (sizeof(numbers) / sizeof(numbers[0])); i++) {
+    printf("Number %d: %d\n", i, numbers[i]);
+  }
+
+  char ca[5];
+  ca[0] = 'D';
+  ca[1] = 'H';
+  ca[2] = 'K';
+  ca[3] = 'E';
+  ca[4] = 'I';
+  printf("%c %c %c %c %c\n", *(ca + 0), *(ca + 1), *(ca + 2), *(ca + 3),
+         *(ca + 4));
+
+  return 0;
 }
--- function_chapter.c
+++ function_chapter.c
@@ -1,16 +1,34 @@
 #include <stddef.h>
 #include <stdio.h>
-void print_big(int argument) {
-  if (argument > 10) {
-    printf("%d is big\n",argument);
-  }
+// void print_big(int argument) {
+//   if (argument > 10) {
+//     printf("%d is big\n", argument);
+//   }
+// }
+// int main(int argc, char *argv[]) {
+//   int array[] = {1, 11, 2, 22, 3, 33};
+//   int i;
+//   int arrayLength = sizeof(array) / sizeof(array[0]);
+//   for (i = 0; i < arrayLength; i++) {
+//     print_big(array[i]);
+//   }
+//   return 0;
+// }
+
+char add(int a, int b);
+
+int main() {
+
+  int num = add(150, 50);
+
+  char v = 43 + 32;
+  printf("%d\n", num);
+  printf("%c\n", v);
 }
-int main(int argc, char *argv[]) {
-  int array[] = {1, 11, 2, 22, 3, 33};
-  int i;
-  int arrayLength = sizeof(array) / sizeof(array[0]);
-  for (i = 0; i < arrayLength; i++) {
-    print_big(array[i]);
-  }
-  return 0;
+
+char add(int a, int b) {
+
+  int c = a + b;
+
+  return c;
 }
